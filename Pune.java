b27885baa61792fbafc2285f5c53a5f7e1f26317
Rajaram <rajatrout914@gmail.com>
2026-03-06 15:05:10 +0530

    m2 method Pune class modified....

--- Pune.java
+++ Pune.java
@@ -3,9 +3,10 @@ public int m1()
 	System.out.println("m1 method exeuted...");
 	return 15;
 }
-public String m2()
+public Stream<String> m2()
 {
 		System.out.println("m2 method exeuted...");
-		return "RAZA";
-
+		String[] str=new String[]{"Tenu","Pannu","Ranuu"};
+		Stream<String> stream2 =Arrays.stream(str);
+        return stream2;
 }
